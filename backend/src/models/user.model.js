b24b0eed65645ce5c39c9b1f8e67014b96d04f2b
benil09 <benil8235@gmail.com>
2026-04-02 01:45:50 +0530

    FEAT: Updated the trigger function to run only if there is a password modification and not when other fields are updated

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,70 +1,74 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import { USER_ROLE } from "../lib/constants.js";
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        validate:{
-            validator:function(value){
-                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                return emailRegex.test(value);
-            },
-            message:props=>`${props.value} is not a valid email address!`   
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(value);
         },
-        lowercase:true,
-        trim:true
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+      lowercase: true,
+      trim: true,
     },
-    isEmailVerified:{
-        type:Boolean,
-        default:false
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
     },
-    dateOfBirth:{
-        type:Date,
-        required:true
+    dateOfBirth: {
+      type: Date,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true,
-        minLength:6
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
     },
-    role:{
-        type:String,
-        enum:{
-            values:Object.values(USER_ROLE),
-            message:`Role must be one of ${Object.values(USER_ROLE).join(", ")}`
-        },
-        default:USER_ROLE.VIEWER
+    role: {
+      type: String,
+      enum: {
+        values: Object.values(USER_ROLE),
+        message: `Role must be one of ${Object.values(USER_ROLE).join(", ")}`,
+      },
+      default: USER_ROLE.VIEWER,
     },
-    isDeleted:{
-        type:Boolean,
-        default:false
+    refreshToken: {
+      type: String,
+      default: null,
     },
-    deletedBy:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        default:null
-    }
-},{
-    timestamps:true
-})
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 
-userSchema.pre('save',async function(){
-    //trigger which is called before saving to encrypt the plain password
-    const hash = await  bcrypt.hash(this.password,10);
-    this.password = hash
-})
+// Only hash when password field is actually changed, preventing double-hash on unrelated saves
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
-userSchema.methods.isValidPassword = async function(password){
-    const currentUser = this;
-    const compare = await bcrypt.compare(password,currentUser.password)
-    return compare;
-}
+userSchema.methods.isValidPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
